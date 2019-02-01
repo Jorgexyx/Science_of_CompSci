b1cc77e6f60fb65b9876648d217ca9a35af0e7b3
jorgexyx <bautisjo@sonoma.edu>
2019-02-01 05:13:33 +0000

    added more data points and labels

--- getTimeAndSample.sh
+++ getTimeAndSample.sh
@@ -1,16 +1,20 @@
 #!/bin/bash
 
-echo "Time" "Sampling Rate"
+echo "Time" "Sampling_Rate"
 for i in `seq 1 20`;
 do
-output="$(./FIRESTARTER -t 2 -l 100 -b 3 -r 2>/dev/null| grep runtime)"
-timeTaken="$( echo $output | cut -d" " -f2)"
-cyclesTakenWithParen="$( echo $output | cut -d"(" -f2)"
-cyclesTaken="$( echo $cyclesTakenWithParen | cut -d" " -f1)"
-samplingRate="$(echo $cyclesTaken / $timeTaken  | bc)"
+	for j in `seq 1  5`
+	do
+		output="$(./FIRESTARTER -t $i -l 100 -b 3 -r 2>/dev/null| grep runtime)"
+		timeTaken="$( echo $output | cut -d" " -f2)"
+		cyclesTakenWithParen="$( echo $output | cut -d"(" -f2)"
+		cyclesTaken="$( echo $cyclesTakenWithParen | cut -d" " -f1)"
+		samplingRate="$(echo $cyclesTaken / $timeTaken  | bc)"
 
-#echo "${output}"
-#echo "${cyclesTaken}"
+		#echo "${output}"
+		#echo "${cyclesTaken}"
 
-echo "${timeTaken}" "${samplingRate}"
+
+		echo "${timeTaken}" "${samplingRate}"
+	done
 done
