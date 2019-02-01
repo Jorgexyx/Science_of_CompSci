d5579a14c0f7a0f0e4b85dcc16c3153831bcb56f
Jorge Bautista <bautisjo@sonoma.edu>
2019-02-01 21:21:39 +0000

    Update graph.R

--- graph.R
+++ graph.R
@@ -1,4 +1,6 @@
 data<-read.table("file.txt", header=TRUE)
 x = data$Time
 y = data$Sampling_Rate
+jpeg("img.jpg")
 plot(x,y, xlab="Wall-clock Peformance", ylab="Sampling Rate", main="Wall-Clock Peformance vs Sampling Rate")
+dev.close
