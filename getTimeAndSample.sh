99d6d3acaa9c0f63d97f33dcced9642c16280c12
jorgexyx <bautisjo@sonoma.edu>
2019-02-01 04:25:42 +0000

    update

--- getTimeAndSample.sh
+++ getTimeAndSample.sh
@@ -4,7 +4,9 @@ printf "\n--------------------------\n"
 echo "RUNNING FIRESTARTER"
 echo "--------------------------"
 
-output="$(./FIRESTARTER -t 10 -l 100 -b 3 -r | grep runtime)"
+for i in `seq 1 20`;
+do
+output="$(./FIRESTARTER -t 2 -l 100 -b 3 -r 2>/dev/null| grep runtime)"
 timeTaken="$( echo $output | cut -d" " -f2)"
 cyclesTakenWithParen="$( echo $output | cut -d"(" -f2)"
 cyclesTaken="$( echo $cyclesTakenWithParen | cut -d" " -f1)"
@@ -14,3 +16,4 @@ samplingRate="$(echo $cyclesTaken / $timeTaken  | bc)"
 #echo "${cyclesTaken}"
 
 echo "${timeTaken}" "${samplingRate}"
+done
