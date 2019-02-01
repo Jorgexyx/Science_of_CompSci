0f2f1acb7ca55069b671e53981244bb78816d645
Jorge Bautista <bautisjo@sonoma.edu>
2019-02-01 21:21:47 +0000

    Update graph.R

--- graph.R
+++ graph.R
@@ -3,4 +3,4 @@ x = data$Time
 y = data$Sampling_Rate
 jpeg("img.jpg")
 plot(x,y, xlab="Wall-clock Peformance", ylab="Sampling Rate", main="Wall-Clock Peformance vs Sampling Rate")
-dev.close
+dev.close()
